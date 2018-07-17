13ea183d9a437c85a0adccf2e88641b1f60da6a8
Kushal Kuchibhotla <98kkushal@gmail.com>
2018-07-16 18:16:20 -0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,31 @@
+
+
+
+var app = angular.module('todo', []); 
+app.controller('todocontrol', function($scope) {
+    $scope.todoLista = [];
+
+    $scope.check = function() {
+        $scope.todoLista.push({todoText:$scope.todoTask, description:$scope.description});
+        console.log($scope.todoLista);
+        $scope.todoInput = "";
+        
+    };
+
+    $scope.remove = function(){
+            var List=[];
+            angular.forEach($scope.todoLista,function(v){
+            if(!v.done){
+                List.push(v);
+            }
+        });    $scope.todoLista=List;
+        };
+});
+// 	$scope.todoList=[]};
+
+// 	$scope.todoAdd=function(){
+// 	$scope.todoList=[{todoTask:$scope.todoTask,description:$scope.description}]
+// 	$scope.todoTask="";
+// 	};
+// });
+
